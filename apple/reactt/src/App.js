5ea0f356be277a2d0cdb9ced156351df7b4e6966
yang <diddnjswns20@gmail.com>
2023-08-10 20:51:27 +0900

    [apple, js] eslint, prettier, 다시,,.,

--- apple/reactt/src/App.js
+++ apple/reactt/src/App.js
@@ -1,17 +1,13 @@
-/*eslint-disable*/ //warning 무시
-
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-    let [name, setname] = useState([
-        "남자 코트 추천",
-        "강남 우동 맛집",
-        "고라니..",
-    ]);
-    let [good, setgood] = useState([0, 1, 2]);
-    let [modal, setmodal] = useState(false);
+    const [name, setname] = useState(["남자 코트 추천", "강남 우동 맛집", "고라니.."]);
+    const [good, setgood] = useState([0, 1, 2]);
+    const [modal, setmodal] = useState(false);
+    const [title, settitle] = useState(0);
+    const [add, setadd] = useState("");
 
     return (
         <div className="App">
@@ -22,16 +18,18 @@ function App() {
                 return (
                     <div className="list" key={i}>
                         <h4
-                        // onClick={() => {
-                        //     setmodal(true);
-                        // }}
+                            onClick={() => {
+                                setmodal(true);
+                                settitle(i);
+                            }}
                         >
                             {name[i]}
                             <span
-                                onClick={() => {
-                                    let copy = [...good];
+                                onClick={(e) => {
+                                    const copy = [...good];
                                     copy[i] = copy[i] + 1;
                                     setgood(copy);
+                                    e.stopPropagation();
                                 }}
                             >
                                 👍
@@ -39,40 +37,59 @@ function App() {
                             {good[i]}
                         </h4>
                         <p>2월 17일 발행</p>
+                        <button
+                            onClick={() => {
+                                let copy = [...name];
+                                copy.splice(i, 1);
+                                setname(copy);
+                            }}
+                        >
+                            삭제
+                        </button>
                     </div>
                 );
             })}
-
-            <button
+            {/* <button
                 onClick={function () {
-                    let copy = [...name];
-                    copy[0] = "여자 코트 추천";
-                    setname(copy);
-                }}
-            >
-                여자 코트 버튼
-            </button>
-            <button
-                onClick={function () {
-                    let copy = [...name];
+                    const copy = [...name];
                     copy.sort();
                     setname(copy);
                 }}
             >
                 가나다순 정렬
+            </button> */}
+            <input type="text" onChange={(e) => setadd(e.target.value)}></input>
+            <button
+                onClick={() => {
+                    let copy = [...name];
+                    copy[copy.length] = add;
+                    setname(copy);
+                }}
+            >
+                버튼
             </button>
-            {modal == true ? <Modal></Modal> : null}
+
+            {modal == true ? <Modal name={name} setname={setname} title={title}></Modal> : null}
         </div>
     );
 }
 
-function Modal() {
+function Modal(props) {
     return (
         <>
             <div className="modal">
-                <h4>제목</h4>
+                <h4>{props.name[props.title]}</h4>
                 <p>날짜</p>
                 <p>상세내용</p>
+                <button
+                    onClick={function () {
+                        const copy = [...props.name];
+                        copy[0] = "여자 코트 추천";
+                        props.setname(copy);
+                    }}
+                >
+                    여자 코트 버튼
+                </button>
             </div>
         </>
     );
