a10503d80d5556442c35ad07d89d3ed207098892
yang <diddnjswns20@gmail.com>
2023-08-26 20:38:44 +0900

    ,

--- apple/shop/src/data/data.js
+++ apple/shop/src/data/data.js
@@ -4,6 +4,7 @@ const data = [
         title: "White and Black",
         content: "Born in France",
         price: 120000,
+        count: 0,
         img: "https://codingapple1.github.io/shop/shoes1.jpg"
     },
 
@@ -12,14 +13,15 @@ const data = [
         title: "Red Knit",
         content: "Born in Seoul",
         price: 110000,
+        count: 0,
         img: "https://codingapple1.github.io/shop/shoes2.jpg"
     },
-
     {
         id: 2,
         title: "Grey Yordan",
         content: "Born in the States",
         price: 130000,
+        count: 0,
         img: "https://codingapple1.github.io/shop/shoes3.jpg"
     }
 ]
