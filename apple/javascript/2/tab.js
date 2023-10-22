5de1b814474608d14d6aace77ee77951d0fae33b
yang <diddnjswns20@gmail.com>
2023-10-22 17:52:16 +0900

    .

--- apple/javascript/2/tab.js
+++ apple/javascript/2/tab.js
@@ -1,24 +1,6 @@
-$(".list").click(function(e){
+$(".list").click(function (e) {
     tab(e.target.dataset.id);
 })
-
-// $(".list").click(function (e) {
-//     if (e.target == document.querySelectorAll(".tab-button")[0]) {
-//         tab(0);
-//     }
-//     if (e.target == document.querySelectorAll(".tab-button")[1]) {
-//         tab(1);
-//     }
-//     if (e.target == document.querySelectorAll(".tab-button")[2]) {
-//         tab(2);
-//     }
-// });
-
-// for (let i = 0; i < tabBtn.length; i++) {
-//     tabBtn.eq(i).on("click", function () {
-//         tab(i);
-//     });
-// }
 
 let tabBtn = $(".tab-button");
 let content = $(".tab-content");
