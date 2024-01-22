501a0952cd70258c6a514b1d51ce7377e681d18c
yangwonjoon <diddnjswns20@gmail.com>
2024-01-23 07:35:10 +0900

    [react] ~ lifecycle과 useeffect 2

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export const Detail = ({ shoes }) => {
 
     let parm = useParams();
     let shoe = shoes.filter(shoe => shoe.id == parm.id)
-    console.log(shoes)
-    console.log(shoe)
-    console.log(parm.id)
+    const [sale, setSale] = useState(true)
+
+    useEffect(() => {
+        let a = setTimeout(() => {
+            setSale(false)
+        }, 2000);
+
+        return () => {
+            clearTimeout(a)
+        }
+    })
 
 
     return (
         <div className="container">
+            {
+                sale
+                    ? <div className="alert alert-warning">
+                        2초내에 구매시 할인
+                    </div>
+                    : null
+            }
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
