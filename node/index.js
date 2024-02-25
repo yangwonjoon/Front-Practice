0f98ba17d0cbb6b5c287c803384f035d1b12b457
yangwonjoon <diddnjswns20@gmail.com>
2024-02-25 10:30:09 +0900

    [react] 4-2

---
+++ node/index.js
@@ -0,0 +1,15 @@
+// const calc = require('./calc.js')
+// const randomColor = require('randomcolor')
+
+
+// console.log(randomColor())
+
+// import { PI, getArea, getCircumference } from './circle.js'
+
+// console.log(PI, getArea(1), getCircumference(1));
+
+
+import * as circle from "./circle.js";
+
+console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+
