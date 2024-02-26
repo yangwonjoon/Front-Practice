a7c350c05a92ca53b2927ed311624b663b2accd7
yangwonjoon <diddnjswns20@gmail.com>
2024-02-26 17:56:26 +0900

    [react] ~5-2

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
-import Viewer from './component/viewer';
-import Controller from './component/controller';
 import { useState } from 'react';
+
+import { DiaryEditor } from './component/diaryeditor';
 
 function App() {
 
-  const [count, setCount] = useState(0)
-
-  const clickHandler = (num) => {
-    setCount(count + num)
-  }
 
   return (
     <div className="App">
-      <h1>Simple Counter</h1>
-      <section>
-        <Viewer count={count} />
-      </section>
-      <section>
-        <Controller clickHandler={clickHandler} />
-      </section>
+      <DiaryEditor></DiaryEditor>
     </div>
   );
 }
