98a234ef1f60ea883c23842cf8c42ed2f5e7d661
yangwonjoon <diddnjswns20@gmail.com>
2024-02-26 17:02:11 +0900

    [react] ~4.5

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Viewer from './component/viewer';
+import Controller from './component/controller';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const clickHandler = (num) => {
+    setCount(count + num)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller clickHandler={clickHandler} />
+      </section>
     </div>
   );
 }
