da6a90b668bcae247e6b88862f4fa8232baa95d3
yangwonjoon <diddnjswns20@gmail.com>
2024-03-04 16:02:51 +0900

    [ts] ~11.1

---
+++ ts/section11/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+type Todos = {
+  id: number;
+  content: string
+}
+
+function App() {
+
+  const [text, setText] = useState('')
+  const [todos, setTodos] = useState<Todos[]>([])
+
+  const onchangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+    console.log(text)
+  }
+
+  const onclickBtn = () => { }
+
+  return (
+    <div className="App">
+      <h1>todo list</h1>
+      <input value={text} onChange={onchangeInput}></input>
+      <button>추가하기</button>
+    </div>
+  );
+}
+
+export default App;
