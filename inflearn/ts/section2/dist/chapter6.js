3846d375d81db0e80f667d01537751219e2ca9df
yang <diddnjswns20@gmail.com>
2024-05-25 21:39:47 +0900

    change folder structure

--- inflearn/ts/section2/dist/chapter6.js
+++
@@ -1,5 +0,0 @@
-let anyVar = 10;
-anyVar = "hello";
-let num = 10;
-num = anyVar;
-export {};
--- inflearn/ts/section2/src/chapter6.ts
+++
@@ -1,7 +0,0 @@
-let anyVar: any = 10;
-anyVar = "hello";
-
-let num: number = 10;
-num = anyVar;
-
-console.log(num)
